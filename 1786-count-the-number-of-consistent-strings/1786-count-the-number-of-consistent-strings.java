bdfe604e735be9ee6501599a7f2bad142799f023
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2024-09-12 09:02:28 +0530

    Time: 12 ms (63.71%), Space: 45 MB (92.85%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.java
@@ -5,17 +5,13 @@ class Solution {
         for(int j = 0; j < allowed.length(); j++)
                 allow.add(allowed.charAt(j));
         for(int i = 0; i < words.length; i++){
-            Set<Character> word = new HashSet<>();
-            for(int j = 0; j < words[i].length(); j++)
-                word.add(words[i].charAt(j));
-            if(word.size() > allowed.length())
-                continue;
             boolean f = true;
-            for(char w : word) 
-                if(!(allow.contains(w))){
+            for(int j = 0; j < words[i].length(); j++)
+                if(!(allow.contains(words[i].charAt(j)))){
                     f = false;
                     break;
                 }
+                
             if(f)
                 c++;           
         }
