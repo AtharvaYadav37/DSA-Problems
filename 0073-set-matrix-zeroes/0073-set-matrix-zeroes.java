517a063a011c44e587c8e0e58f7248c39f156b9d
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2024-09-13 21:35:12 +0530

    Time: 2 ms (18.81%), Space: 45.8 MB (18.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -10,16 +10,29 @@ class Solution {
                 }
         
             
-
-        for(int i : row){
-            for(int j = 0; j < matrix[0].length; j++)
-                matrix[i][j] = 0;
+        if(row.size() == matrix.length){
+            for(int i : row){
+                for(int j = 0; j < matrix[0].length; j++)
+                    matrix[i][j] = 0;
+            }
         }
 
-        for(int j : col){
-            for(int i = 0; i < matrix.length; i++)
-                matrix[i][j] = 0;
+        else if(col.size() == matrix[0].length){
+            for(int j : col){
+                for(int i = 0; i < matrix.length; i++)
+                    matrix[i][j] = 0;
+            }
         }
-        
+
+        else{
+            for(int i : row){
+                for(int j = 0; j < matrix[0].length; j++)
+                    matrix[i][j] = 0;
+            }
+            for(int j : col){
+                for(int i = 0; i < matrix.length; i++)
+                    matrix[i][j] = 0;
+            }
+        }
     }
 }
