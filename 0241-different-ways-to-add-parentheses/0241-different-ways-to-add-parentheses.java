9b2f1093b45b83d6733ef5ce982336aae0acf8e4
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2024-09-19 21:37:17 +0530

    Time: 2 ms (68.85%), Space: 42.3 MB (60.69%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -36,3 +36,49 @@ class Solution {
         return 0;
     }
 }
+
+
+/*
+    class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = expression.length();
+        for(int i = 0; i < n-1; i++){
+            char c = expression.charAt(i);
+            if(c == '+' || c == '-' || c == '*'){
+                int n1 = expEval(expression.substring(0, i));
+                int n2 = expEval(expression.substring(i+1, n));
+                arr.add(task(n1, c, n2));
+            }
+        }
+        return arr;
+    }
+
+    public int expEval(String s){
+        int n = 0;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '+' || c == '-' || c == '*'){
+                n = i;
+                break;
+            }
+        }
+        if(n > 0 && n < s.length()){
+            int n1 = expEval(s.substring(0, n));
+            int n2 = expEval(s.substring(n+1, s.length()));
+            return task(n1, s.charAt(n), n2);
+        }
+            
+        return Integer.valueOf(s);
+    }
+
+    public int task(int n1, char c, int n2){
+        switch(c){
+            case '+' : return n1 + n2;
+            case '-' : return n1 - n2;
+            case '*' : return n1 * n2;
+        }
+        return 0;
+    }
+}
+*/
