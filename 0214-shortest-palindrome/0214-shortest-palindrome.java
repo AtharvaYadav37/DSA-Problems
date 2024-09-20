e301844e7c65b1beccff19a7bb8fa0b9549e743b
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2024-09-20 23:43:41 +0530

    Time: 35 ms (44%), Space: 45.3 MB (14.54%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,27 +1,14 @@
 class Solution {
     public String shortestPalindrome(String s) {
-        if (s.length() == 0 || s.length() == 1)
+        if(s.length() == 0 || s.length() == 1)
             return s;
-
-        int i = 0;
-        int j = s.length() - 1;
-        int end = j;  // Variable to track the end of the palindrome
-
-        while (i < j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
-            } else {
-                // Reset i and decrease end to try finding a shorter palindrome from the start
-                i = 0;
-                end--;
-                j = end;  // Set j to the new end
+        int n = s.length();
+        String rev = new StringBuilder(s).reverse().toString();
+        for(int i = 0; i < s.length(); i++){
+            if(s.startsWith(rev.substring(i))){
+                return rev.substring(0, i) + s;
             }
         }
-
-        // If the palindrome ends before the end of the string, append the missing part in reverse
-        String suffix = s.substring(end + 1); 
-        String prefix = new StringBuilder(suffix).reverse().toString();
-        return prefix + s;
+        return rev + s;
     }
 }
