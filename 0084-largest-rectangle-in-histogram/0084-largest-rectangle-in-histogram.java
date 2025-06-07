1765b2734f41d9627ffb4f34920f5b0e9587fa10
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2025-06-07 14:58:27 +0530

    Time: 69 ms (60.88%), Space: 57.5 MB (53.24%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -5,16 +5,16 @@ class Solution {
         int len = heights.length;
 
         for(int i = 0; i <= len; i++){
-            int length = (i == heights.length) ? 0 : heights[i];
+            int length = (i == heights.length) ? 0 : heights[i];        //on reaching end of array see all possible combinations
             
             while(!stack.isEmpty() && length < heights[stack.peek()]){
-                int pop = stack.pop();
-                int height = heights[pop];
-                int breadth = stack.isEmpty() ? i : i - stack.peek() - 1;
+                int pop = stack.pop();                                  //the last smallest value's index
+                int height = heights[pop];                              //height of that value
+                int breadth = stack.isEmpty() ? i : i - stack.peek() - 1;       //if empty then move all back to 0 index
                 maxArea = Math.max(maxArea, height * breadth);
             }
 
-            stack.push(i);
+            stack.push(i);                                              //push the current index
         }        
 
         return maxArea;
