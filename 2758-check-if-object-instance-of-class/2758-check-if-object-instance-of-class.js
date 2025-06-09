c3c6b326e9a9b96ccfb53d240013e6c124341104
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2025-06-09 16:23:04 +0530

    Time: 72 ms (55.87%), Space: 62 MB (85.35%) - LeetHub

---
+++ 2758-check-if-object-instance-of-class/2758-check-if-object-instance-of-class.js
@@ -0,0 +1,16 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+
+    if(obj != null && typeof classFunction === 'function' && Object(obj) instanceof classFunction){
+        return true;
+    }
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
