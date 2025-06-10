83fd3a3158c790c433a6bf0eeccfeb29ca32a589
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2025-06-10 10:24:23 +0530

    Time: 354 ms (2.45%), Space: 42.3 MB (53.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if((x == -1 && n % 2 == 0) || (x == 1) || (n == 0))
+            return 1;
+        if(x == -1 && n % 2 == 1)
+            return -1;
+        if((n <= Integer.MIN_VALUE && x > 1) || (n >= Integer.MAX_VALUE && x < 1) || (x == 0))
+            return 0;
+        if(n < 0)
+            x = 1/x;
+        double pow = 1;
+        for(int i = 0; i < Math.abs(n); i++){
+            if(pow >= -10000 && pow <= 10000)
+                pow = pow*x;
+            else
+                break;
+        }
+        return pow;
+    }
+}
