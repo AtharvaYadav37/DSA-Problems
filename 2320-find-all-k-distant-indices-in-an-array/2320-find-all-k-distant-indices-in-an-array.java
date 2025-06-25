7da3587d007ec1d87713d295899f87a77c3548b5
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2025-06-25 20:47:36 +0530

    Time: 17 ms (38.5%), Space: 44.9 MB (48.66%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,31 +1,45 @@
+// class Solution {
+//     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+//         HashSet<Integer> indexes = new HashSet<>();
+//         int len = nums.length;
+
+//         for(int i = 0; i < len; i++){
+//             if(nums[i] == key){
+//                 for(int j = i; j <= i + k && j < len; j++){
+//                     indexes.add(j);
+//                 }
+//                 for(int j = i; j >= i - k && j >= 0; j--){
+//                     indexes.add(j);
+//                 }
+//             }
+//         }
+
+//         List<Integer> ret = new ArrayList<>(indexes);
+
+//         Collections.sort(ret);
+
+//         return ret;
+//     }
+// }
+
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        HashSet<Integer> indexes = new HashSet<>();
-        int len = nums.length;
-
-        for(int i = 0; i < len; i++){
-            if(nums[i] == key){
-                for(int j = i; j <= i + k && j < len; j++){
-                    indexes.add(j);
-                }
-                for(int j = i; j >= i - k && j >= 0; j--){
-                    indexes.add(j);
+        int count = 0;
+        boolean[] bool = new boolean[nums.length];
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i]==key){
+                for(int j = i-k; j < i+k+1 && j < nums.length; j++){
+                    if(j < 0){
+                        continue;
+                    }
+                    if(bool[j] == false){
+                        arr.add(j);
+                        bool[j] = true;
+                    }
                 }
             }
         }
-
-        // for(int i = len - 1; i >= 0; i--){
-        //     if(nums[i] == key){
-        //         for(int j = i; j >= i - k && j >= 0; j--){
-        //             indexes.add(j);
-        //         }
-        //     }
-        // }
-
-        List<Integer> ret = new ArrayList<>(indexes);
-
-        Collections.sort(ret);
-
-        return ret;
+        return arr;
     }
 }
