7dc78c9b35f76a2a845a385c5613294916220646
Atharva Kumar Yadav <atharvayadav1441@gmail.com>
2025-10-02 12:08:55 +0530

    Time: 22 ms (36.88%), Space: 45.5 MB (24.2%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -6,28 +6,28 @@ class Solution {
         int low = 0;
         while(low <= high){
             int mid = (low + high) / 2;
-            int k = calculateHours(piles, mid, h);
+            boolean k = calculateHours(piles, mid, h);
 
-            if(k == -1)
-                high = mid - 1;
-            else
+            if(k)
                 low = mid + 1;
+            else
+                high = mid - 1;
         }
 
         return low;
     }
 
-    static int calculateHours(int[] piles, int k, int h){
+    static boolean calculateHours(int[] piles, int k, int h){
         int len = piles.length;
         int i = 0;
         int totalHours = 0;
 
         while(i < len){
             totalHours = totalHours + (int)(Math.ceil(piles[i] / (double)k));
-            if(totalHours > h)  return 1;
+            if(totalHours > h)  return true;
             ++i;
         }
 
-        return -1;
+        return false;
     }
 }
